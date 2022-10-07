c77edb9a0795e8d16ba7f2a999830a78c423d18d
Daiki Hirabayashi <gigabowser@hotmail.co.jp>
2022-10-07 21:47:22 +0900

    型、連想配列のサンプルを追加

---
+++ ch2/hashmap_chk.rs
@@ -0,0 +1,14 @@
+use std::collections::HashMap;
+
+fn main() {
+    let mut map = HashMap::new();
+    map.insert("A", 30);
+    map.insert("B", 50);
+
+    // キーが存在しないか確認する
+    if map.get("D") == None {
+        println!("Dは存在しない");
+    } else {
+        println!("D={}", map["D"]);
+    }
+}
